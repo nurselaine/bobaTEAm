37b3c08d316a9b1b2cb96975741d720855aab584
macnoblebk <macnoblebk@gmail.com>
2024-05-06 12:55:55 -0700

    Creates User model

--- ContosoCrafts.WebSite/Models/User.cs
+++ ContosoCrafts.WebSite/Models/User.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
 namespace ContosoCrafts.WebSite.Models
 {
 	public class User
 	{
-		public User()
-		{
-		}
-	}
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime JoinedAt { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string ProfilePictureUrl { get; set; } // URL to profile picture or avatar
+
+
+        public override string ToString() => JsonSerializer.Serialize<User>(this);
+    }
 }
 
