fe509ad3b7f7dc768dcf724ad03d25eecaf5e167
nurselaine <Elaine.t.huynh@gmail.com>
2024-05-21 02:08:37 -0700

    Latest commit

--- ContosoCrafts.WebSite/Models/Order.cs
+++ ContosoCrafts.WebSite/Models/Order.cs
@@ -21,32 +21,35 @@ namespace ContosoCrafts.WebSite.Models
 		private const double SalesTax = 0.09;
 		public string TimeStamp { get; set; }
 		public MenuItem Item { get; set; }
-		public string Size { get; set; }
-		public MenuItem[] Sides { get; set; }
-		public string IceLevel { get; set; }
-		public string SugarLevel {  get; set; }
+		public string? Size { get; set; }
+		public MenuItem?[] Sides { get; set; }
+		public string? IceLevel { get; set; }
+		public string? SugarLevel {  get; set; }
 		public double OrderTotal { get; set; }
 		public double OrderSubtotal { get; set; }
 
 		public double CalculateOrderSubtotal()
 		{
 			
+			double total = 0.0;
 			if (Item.Price == null)
 			{
 				Item.Price = "$10.0";
 			}
-			double total = 0.0;
-			foreach (MenuItem item in Sides)
+			if(Sides != null)
 			{
-				if(item.Price == null)
+				foreach (MenuItem item in Sides)
 				{
-					item.Price = "$3.00";
+					if(item.Price == null)
+					{
+						item.Price = "$3.00";
+					}
+					// remove dollar sign
+					string price = item.Price.Substring(1);
+					// convert to double
+					double sidePrice = Convert.ToDouble(price);
+					total += sidePrice;
 				}
-				// remove dollar sign
-				string price = item.Price.Substring(1);
-				// convert to double
-				double sidePrice = Convert.ToDouble(price);
-				total += sidePrice;
 			}
 			// remove dollar sign
 			string? productPrice = Item.Price.Substring(1);
