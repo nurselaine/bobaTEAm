53e7c45d1103777cb48752efccc3a6294dcc52d8
Crazypandaz <rockmanjason@gmail.com>
2024-05-28 16:16:32 -0700

    -Added JsonFileReviewService to Startup.cs.

--- ContosoCrafts.WebSite/Startup.cs
+++ ContosoCrafts.WebSite/Startup.cs
@@ -28,6 +28,7 @@ namespace ContosoCrafts.WebSite
             services.AddTransient<JsonFileProductService>();
             services.AddTransient<JsonFileMenuServices>();
             services.AddTransient<JsonFileOrderService>();
+            services.AddTransient<JsonFileReviewService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
