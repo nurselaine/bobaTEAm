f518a78531b38b313986446dd8ac4b7326b8d6a9
Crazypandaz <rockmanjason@gmail.com>
2024-05-28 16:09:29 -0700

    -Added Review model file to represent review object in review.json.

---
+++ ContosoCrafts.WebSite/Models/Review.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContosoCrafts.WebSite.Models
+{
+	public class Review
+	{
+		public string? Id { get; set; }
+
+		public string? Username { get; set; }
+
+		public string? Title { get; set; }
+
+		public string? Description { get; set; }
+
+		public string? ProductId { get; set; }
+		public string? UserId { get; set; }
+
+		public int Rating { get; set; }
+
+		public DateTime? CreateDate { get; set; }
+
+		public override string ToString()
+		{
+			return JsonSerializer.Serialize<Review>(this);
+		}
+	}
+
+}
