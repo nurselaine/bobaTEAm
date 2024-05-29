0a0dff7bd99765d4e7c6b116fe5321222ff5ba23
Crazypandaz <rockmanjason@gmail.com>
2024-05-28 20:06:01 -0700

    -Added model validation rules to Review model file.

--- ContosoCrafts.WebSite/Models/Review.cs
+++ ContosoCrafts.WebSite/Models/Review.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -10,15 +11,26 @@ namespace ContosoCrafts.WebSite.Models
 	{
 		public string? Id { get; set; }
 
+		[Required]
+		[StringLength(25, MinimumLength = 3,
+			ErrorMessage = "{0} length must be between {2} and {1}." )]
 		public string? Username { get; set; }
 
+		[Required]
+		[StringLength(50, MinimumLength = 10,
+			ErrorMessage = "{0} length must be between {2} and {1}.")]
 		public string? Title { get; set; }
 
+		[Required]
+		[StringLength(500, MinimumLength = 10,
+			ErrorMessage = "{0} length must be between {2} and {1}.")]
 		public string? Description { get; set; }
 
 		public string? ProductId { get; set; }
 		public string? UserId { get; set; }
 
+		[Required]
+		[Range(1, 5)]
 		public int Rating { get; set; }
 
 		public DateTime? CreateDate { get; set; }
