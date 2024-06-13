e4989927c6e01ba4592b7eecacdfcf57d0d21d9a
nurselaine <Elaine.t.huynh@gmail.com>
2024-06-13 10:22:52 -0700

    FIX: Register User page and BobaService updates boba users json file

--- ContosoCrafts.WebSite/Services/BobaUserService.cs
+++ ContosoCrafts.WebSite/Services/BobaUserService.cs
@@ -3,32 +3,52 @@ using System.IO;
 using System.Text.Json;
 using Microsoft.AspNetCore.Hosting;
 using ContosoCrafts.WebSite.Models;
+using System.Linq;
 
 namespace ContosoCrafts.WebSite.Services
 {
 	public class BobaUserService
 	{
         private readonly string filePath;
+        private string bobaUserFileName;
 
         public BobaUserService(IWebHostEnvironment webHostEnvironment)
         {
-            filePath = Path.Combine(webHostEnvironment.WebRootPath, "data", "bobausers.json");
+            bobaUserFileName = Path.Combine(webHostEnvironment.WebRootPath, "data", "bobausers.json");
         }
 
         public List<BobaUser> LoadUsers()
         {
-            if (!File.Exists(filePath))
+            if (!File.Exists(bobaUserFileName))
                 return new List<BobaUser>();
 
-            string json = File.ReadAllText(filePath);
+            string json = File.ReadAllText(bobaUserFileName);
             return JsonSerializer.Deserialize<List<BobaUser>>(json) ?? new List<BobaUser>();
         }
 
-        public void SaveUsers(List<BobaUser> users)
+        public BobaUser CreateData(BobaUser user)
         {
-            var options = new JsonSerializerOptions { WriteIndented = true };
-            string json = JsonSerializer.Serialize(users, options);
-            File.WriteAllText(filePath, json);
+            List<BobaUser> UserList = LoadUsers();
+            UserList.Add(user);
+
+            // save new data in Json file
+            SaveData(UserList);
+
+            return user;
+        }
+
+        public void SaveData(List<BobaUser> bobaUsers)
+        {
+            using var outputStream = File.OpenWrite(bobaUserFileName);
+
+            JsonSerializer.Serialize<IEnumerable<BobaUser>>(
+                new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                {
+                    SkipValidation = true,
+                    Indented = true
+                }),
+                bobaUsers
+            );
         }
     }
 }
